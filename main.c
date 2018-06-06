dd73f6cd806c33c1f26ce4f989a8dab25774ec0b
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-06-06 17:29:15 +0300

    Add files via upload
    Код, который создаёт новый файл с символами, расположенными в порядке возрастания их кода

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+    int main(void) {
+        int ch;
+        FILE * pf_r, * pf_w;
+        int presence[255] = {
+            0
+        };
+
+        pf_r = fopen("old.txt", "r");
+        pf_w = fopen("new.txt", "w");
+        fprintf(stderr, "new.txt");
+
+        for (int i = 0; i < 255; ++i) {
+            while (feof(pf_r) == 0) {
+                ch = getc(pf_r);
+                if (presence[ch] != 1 && ch == i) {
+                    presence[ch] = 1;
+                    putc(ch, pf_w);
+                }
+            }
+            fseek(pf_r, 0L, SEEK_SET);
+        }
+        fclose(pf_r);
+        fclose(pf_w);
+        return 0;
+    }
